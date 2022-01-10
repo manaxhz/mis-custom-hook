49be86607256f873043e8c8be7148ee5aec559f8
Gonzalo Aracena <gonzalo.aracena.p@gmail.com>
2022-01-10 13:55:13 -0300

    Primer commit

---
+++ useCounter.js
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+
+
+export const useCounter = ( initialState = 10 ) => {
+    
+    const [counter, setCounter] = useState(initialState);
+
+
+    const increment = () => {
+        setCounter( c => c + 1);
+    }
+
+    const decrement = () => {
+        setCounter( c => c - 1);
+    }
+
+    const reset = () => {
+        setCounter( initialState );
+    }
+
+    return {
+        counter,
+        increment,
+        decrement,
+        reset
+    };
+}
---
+++ useFetch.js
@@ -0,0 +1,69 @@
+import { useState, useEffect, useRef } from "react"
+
+export const useFetch = ( url ) => {
+   
+    
+    const isMounted = useRef(true);
+    const [state, setState] = useState( {data: null, loading: true, error: null})
+
+    useEffect(() => {
+        
+        return () => {
+            isMounted.current = false;
+        }
+
+    }, [])
+
+
+    useEffect(() => {
+        
+        setState({
+            data:null,
+            loading: true,
+            error: null
+        });
+
+        fetch( url )
+            .then( resp => resp.json() )
+            .then( data => {
+               
+           
+                // !!isMounted.current && (setState({
+                //     loading: false,
+                //     error: null,
+                //     data
+                // })); 
+
+
+                // isMounted.current ? (setState({
+                //     loading: false,
+                //     error: null,
+                //     data
+                // })) 
+                // :
+                // console.log('No se llamo el setState')
+
+
+                   if ( isMounted.current){
+                    setState({
+                        loading: false,
+                        error: null,
+                        data
+                    });
+                   }
+            
+            } )
+            .catch( () => {
+                setState({
+                    data:null,
+                    loading: false,
+                    error: 'No se pudo cargar la info'
+                })
+            })
+       
+    }, [url])
+
+
+    return state;
+
+}
